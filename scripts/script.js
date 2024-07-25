c6f1cf534ad991cf7c81a49191d817b24c5773fb
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-07-25 14:26:37 -0300

    modal

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,16 @@ function toggleContent(pacoteId) {
 }
 
 
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target.className === "modal") {
+        closeModal(event.target.id);
+    }
+}
